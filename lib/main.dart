1c798f1f5ed7e7b9ec2ab6488f7b9214cbeed6fe
rafaelmaestro <rafaelmaestro@live.com>
2024-10-13 22:05:38 -0300

    feat: adiciona verificacao para saber se o app esta rodando em um emulador ou dispositivo fisico

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_background/flutter_background.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_srpg_app/controllers/posicao_controller.dart';
@@ -15,6 +17,25 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
+
+  var deviceData = <String, dynamic>{};
+  final deviceInfoPlugin = DeviceInfoPlugin();
+
+  bool isEmulator = false;
+  bool validadeEmulator =
+      FlutterConfig.get('VALIDATE_EMULATOR_BOOLEAN') == 'true';
+
+  if (validadeEmulator) {
+    if (GetPlatform.isAndroid) {
+      var androidInfo = await deviceInfoPlugin.androidInfo;
+      deviceData = _readAndroidBuildData(androidInfo);
+      isEmulator = !androidInfo.isPhysicalDevice;
+    } else if (GetPlatform.isIOS) {
+      var iosInfo = await deviceInfoPlugin.iosInfo;
+      deviceData = _readIosDeviceInfo(iosInfo);
+      isEmulator = !iosInfo.isPhysicalDevice;
+    }
+  }
 
   const androidConfig = FlutterBackgroundAndroidConfig(
       notificationTitle: "App em execução",
@@ -39,7 +60,59 @@ void main() async {
         await FlutterBackground.enableBackgroundExecution();
   }
 
-  runApp(const MyApp());
+  if (isEmulator) {
+    runApp(EmulatorWarningApp());
+  } else {
+    runApp(const MyApp());
+  }
+}
+
+Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+  return <String, dynamic>{
+    'version.securityPatch': build.version.securityPatch,
+    'version.sdkInt': build.version.sdkInt,
+    'version.release': build.version.release,
+    'version.previewSdkInt': build.version.previewSdkInt,
+    'version.incremental': build.version.incremental,
+    'version.codename': build.version.codename,
+    'version.baseOS': build.version.baseOS,
+    'board': build.board,
+    'bootloader': build.bootloader,
+    'brand': build.brand,
+    'device': build.device,
+    'display': build.display,
+    'fingerprint': build.fingerprint,
+    'hardware': build.hardware,
+    'host': build.host,
+    'id': build.id,
+    'manufacturer': build.manufacturer,
+    'model': build.model,
+    'product': build.product,
+    'supported32BitAbis': build.supported32BitAbis,
+    'supported64BitAbis': build.supported64BitAbis,
+    'supportedAbis': build.supportedAbis,
+    'tags': build.tags,
+    'type': build.type,
+    'isPhysicalDevice': build.isPhysicalDevice,
+    'systemFeatures': build.systemFeatures,
+  };
+}
+
+Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+  return <String, dynamic>{
+    'name': data.name,
+    'systemName': data.systemName,
+    'systemVersion': data.systemVersion,
+    'model': data.model,
+    'localizedModel': data.localizedModel,
+    'identifierForVendor': data.identifierForVendor,
+    'isPhysicalDevice': data.isPhysicalDevice,
+    'utsname.sysname:': data.utsname.sysname,
+    'utsname.nodename:': data.utsname.nodename,
+    'utsname.release:': data.utsname.release,
+    'utsname.version:': data.utsname.version,
+    'utsname.machine:': data.utsname.machine,
+  };
 }
 
 class MyApp extends StatelessWidget {
@@ -70,6 +143,50 @@ class MyApp extends StatelessWidget {
           GetPage(name: '/historico', page: () => HistoricoEventosPage()),
           GetPage(name: '/perfil', page: () => PerfilPage())
         ],
+      ),
+    );
+  }
+}
+
+class EmulatorWarningApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF0A6D92),
+        body: Center(
+          child: AlertDialog(
+            title: const Text(
+              'Emulador detectado 🚫',
+              textAlign: TextAlign.center, // Centraliza o título
+              style: TextStyle(
+                  fontWeight: FontWeight.bold), // Torna o título em negrito
+            ),
+            content: const Text(
+              'Este aplicativo não pode ser executado em emuladores. Por favor, instale-o em um dispositivo físico.',
+              textAlign: TextAlign.center, // Centraliza o conteúdo
+            ),
+            actions: <Widget>[
+              Center(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor:
+                        Colors.red, // Cor do texto do botão Cancelar
+                    side: const BorderSide(
+                        color: Colors.red, width: 2), // Borda do botão Cancela
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: () {
+                    // Fecha o aplicativo
+                    SystemNavigator.pop();
+                  },
+                  child: const Text('Fechar'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
